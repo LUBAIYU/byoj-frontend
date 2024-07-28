efd56efa3733c478e3430b9a0ec30a3a47d2d02d
honghu <3027708213@qq.com>
2024-07-28 21:57:49 +0800

    测试

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,6 +34,8 @@ router.beforeEach((to, from, next) => {
     role = getRole(token);
     userStore.setRole(role ?? 1);
   }
+
+  
   // 校验是否有访问权限;
   if (to.meta.access && to.meta.access === "admin") {
     if (role !== 0) {
